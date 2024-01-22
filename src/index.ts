9459aeb5a1eba7888d572fc10e241e465ce3b6d8
Saad Ahmad <saadkwi12@hotmail.com>
2024-01-22 23:08:16 +1100

    update

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,17 @@ import {
   ICloudEventHandler,
 } from './CloudEventHandler/types';
 
+import CloudEventRouter from './CloudEventRouter';
+import { ICloudEventRouter } from './CloudEventRouter/types';
+import { PromiseTimeoutError, timedPromise } from './utils';
+
 export {
   CloudEventHandler,
+  CloudEventRouter,
   CloudEventHandlerError,
+  PromiseTimeoutError,
+  timedPromise,
   CloudEventValidationSchema,
   ICloudEventHandler,
+  ICloudEventRouter,
 };
