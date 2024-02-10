efe2d2dcd21ef7ebd240727da054766400dc110d
Saad Ahmad <saadkwi12@hotmail.com>
2024-02-10 17:17:26 +1100

    update

--- src/CloudEventHandler/createHttpHandler.ts
+++ src/CloudEventHandler/createHttpHandler.ts
@@ -1,3 +1,4 @@
+import fetch from "node-fetch";
 import { formatTemplate } from '../utils';
 import createSimpleHandler from './createSimpleHandler';
 import { ICreateHttpCloudEventHandler } from './types';
